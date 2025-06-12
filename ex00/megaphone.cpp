56e67bd22ba029ed1110726ad37052eccac3dd76
tbolsako <tanjutik162@gmail.com>
2025-06-12 20:11:25 +0200

    include libraries that I need for this program; trying to understand how to receive string from the arguments in case of cpp, especially the syntaxis of writing it

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main() {
+	std::string str_1;
+	std::string str_2 = ;
+
+	std::getline()
+
+	for (int i = 0; i < str.length; i++) {
+		std::cout
+	}
+	std::cout
+}
