54696afb4bacea48a126fb552f8f5598a1a41c71
bolsakova <bolsakova@github.com>
2025-06-13 12:28:41 +0200

    changed the approach of ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
-#include <cstring>
-#include <cctype>
+#include <string>
+// #include <cstring>
+// #include <cctype>
+
+// int main(int ac, char *av[]) {
+// 	if (ac != 2) {
+// 		return 1;
+// 	}
+// 	auto str = std::string(av[1]);
+// 	for (auto& symbol : str) {
+// 		if (std::islower(symbol)) {
+// 			symbol = std::toupper(symbol);
+// 		}
+// 		std::cout << symbol;
+// 	}
+// 	std::cout << std::endl;
+// 	return 0;
+// }
 
 int main(int ac, char *av[]) {
-	if (ac != 2) {
+	if (ac != 2)
 		return 1;
-	}
-	auto str = std::string(av[1]);
-	for (auto& symbol : str) {
-		if (std::islower(symbol)) {
-			symbol = std::toupper(symbol);
-		}
-		std::cout << symbol;
+	std::string str = av[1];
+	for (size_t i = 0; i < str.length(); i++) {
+		if (std::islower(str[i]))
+			str[i] = std::toupper(str[i]);
+		std::cout << str[i];
 	}
 	std::cout << std::endl;
 	return 0;
